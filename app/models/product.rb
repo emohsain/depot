b45523a4f1726e3e21cbe909abf88c5d952f0120
emohsain <31906605+emohsain@users.noreply.github.com>
2022-12-16 01:10:05 -0800

    fourthcommit

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,22 @@
+#---
+# Excerpted from "Agile Web Development with Rails 6",
+# published by The Pragmatic Bookshelf.
+# Copyrights apply to this code. It may not be used to create training material,
+# courses, books, articles, and the like. Contact us if you are in doubt.
+# We make no guarantees that this code is fit for any purpose.
+# Visit http://www.pragmaticprogrammer.com/titles/rails6 for more book information.
+#---
+class Product < ApplicationRecord
+  validates :title, :description, :image_url, presence: true
+# the url data was incorrect see fig1 of notes
+  validates :title, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with:    %r{\.(gif|jpg|png)\z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+  validates :price, numericality: { greater_than_or_equal_to: 0.01 } 
+  #the price was entered incorrectly see fig.2 in notes ~Agile2.
+
+end
 class Product < ApplicationRecord
 end
